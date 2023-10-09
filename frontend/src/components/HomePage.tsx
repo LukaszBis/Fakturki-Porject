02f9f59c9cb1ad7ccc25cfcb213b9d0d6a85f219
Damian Kostek <damian.kostek1@wp.pl>
2023-10-09 18:49:44 +0200

    add frontend files

---
+++ frontend/src/components/HomePage.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import "./HomePage.css";
+import user from "../assets/user.png";
+
+interface Invoice {
+  id: number;
+  number: string;
+  date: string;
+  amount: number;
+}
+
+const invoices: Invoice[] = [
+  { id: 1, number: 'FH001', date: '2023-10-09', amount: 1000 },
+  { id: 2, number: 'FH002', date: '2023-10-10', amount: 1500 },
+  { id: 3, number: 'FH003', date: '2023-10-11', amount: 800 },
+];
+
+const HomePage: React.FC = () => {
+  return (
+    <div className='main'>
+      <div className='banner'>
+        <span className='titlePage'>Fakturki Home</span>
+        <span className='companyName'>
+          Nazwa firmy...
+        </span>
+        <span className='signUpIn'>
+          <a href=''><img src={user} alt="user" width={42} height={42}/></a>
+        </span>
+      </div>
+      <div className='addInvoice'>
+        <button>Dodaj nową fakturę</button>
+      </div>
+      <div className='content'>
+        <div className='menu'>
+          <div className='menuButton'>
+            <button>Faktury</button>
+          </div>
+          <div className='menuButton'>
+            <button>Paragony</button>
+          </div>
+          <div className='menuButton'>
+            <button>Zaliczki</button>
+          </div>
+        </div>
+        <div className='invoiceTable'>
+        <h2>Lista Faktur</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Numer Faktury</th>
+              <th>Data</th>
+              <th>Kwota</th>
+            </tr>
+          </thead>
+          <tbody>
+            {invoices.map((invoice) => (
+              <tr key={invoice.id}>
+                <td>{invoice.number}</td>
+                <td>{invoice.date}</td>
+                <td>${invoice.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
