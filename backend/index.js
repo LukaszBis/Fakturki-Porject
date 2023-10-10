622fc5ca8deef0a5f845c6385b4bc055b25dfb0b
Jakub Kurzacz <jakub-kurzacz@wp.pl>
2023-10-10 20:13:44 +0200

    jk - dbconnect

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// Importuj bibliotekę MongoDB
+const mongoose = require('mongoose');
+
+// Adres hosta bazy danych MongoDB na podstawie nazwy usługi w docker-compose.yml
+const dbHost = 'database'; // To jest nazwa usługi bazy danych w docker-compose.yml
+
+// Połączenie z bazą danych
+mongoose.connect(`mongodb://${dbHost}:27017/users`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  auth: {
+    user: 'Fakturki', // Nazwa użytkownika
+    password: 'fakturki1234', // Hasło
+  },
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('Błąd połączenia z bazą danych:', error);
+});
+
+db.once('open', () => {
+  console.log('Połączono z bazą danych MongoDB');
+});
+
 app.get('/', (req, res) => {
-  res.send('Work?!');
+  res.send('Work?! Connected to MongoDB!');
 });
 
 app.listen(port, () => {
