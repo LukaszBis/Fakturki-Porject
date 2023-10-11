ad33c1ae0fc04bb8dd9cc121a9af79b32232ef88
Jakub Kurzacz <jakub-kurzacz@wp.pl>
2023-10-11 20:35:15 +0200

    js - controllers

---
+++ backend/controllers/person.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+// Adres hosta bazy danych MongoDB na podstawie nazwy usługi w docker-compose.yml
+const dbHost = 'database'; // To jest nazwa usługi bazy danych w docker-compose.yml
+// Połączenie z bazą danych
+mongoose.connect(`mongodb://${dbHost}:27017/faktury`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.error('Błąd połączenia z bazą danych:', error);
+});
+db.once('open', () => {
+  console.log('Połączono z bazą danych MongoDB');
+});
+
+const personSchema = new mongoose.Schema({
+    firstName: String,
+    email: String,
+    password: String,
+    postalCode: String,
+    street: String,
+    lastName: String,
+    phoneNumber: Number,
+    city: String,
+    houseNumber: String,
+    apartmentNumber: String,
+});
+const Person = mongoose.model('Person', personSchema);
+
+
+async function add(firstName, email, password, postalCode, street, lastName, phoneNumber, city, houseNumber, apartmentNumber) {
+    try {
+        const person = new Person({ firstName, email, password, postalCode, street, lastName, phoneNumber, city, houseNumber, apartmentNumber });
+        await person.save();
+        console.log('Osoba została dodana do bazy danych.');
+    } catch (error) {
+        console.error('Błąd podczas dodawania osoby:', error);
+    }
+}
+async function displayAll() {
+    try {
+        const persons = await Person.find().exec(); // Pobierz wszystkie osoby z bazy danych
+        console.log('Wszystkie osoby w bazie danych:', persons);
+        return persons;
+    } catch (error) {
+        console.error('Błąd podczas pobierania osób:', error);
+        throw error;
+    }
+}
+
+module.exports = { add,displayAll };
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
 const app = express();
 const port = 8080;
-// Adres hosta bazy danych MongoDB na podstawie nazwy usługi w docker-compose.yml
-const dbHost = 'database'; // To jest nazwa usługi bazy danych w docker-compose.yml
 
 //this is new \/
 app.use(bodyParser.json()) // for parsing application/json
@@ -19,58 +16,12 @@ const corsOptions ={
 }
 //this is new \/
 app.use(cors(corsOptions)) // Use this after the variable declaration
-// Połączenie z bazą danych
-mongoose.connect(`mongodb://${dbHost}:27017/faktury`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', (error) => {
-  console.error('Błąd połączenia z bazą danych:', error);
-});
-db.once('open', () => {
-  console.log('Połączono z bazą danych MongoDB');
-});
 
 
 
 
 
-
-const personSchema = new mongoose.Schema({
-  firstName: String,
-  email: String,
-  password: String,
-  postalCode: String,
-  street: String,
-  lastName: String,
-  phoneNumber: Number,
-  city: String,
-  houseNumber: String,
-  apartmentNumber: String,
-});
-const Person = mongoose.model('Person', personSchema);
-
-
-async function addPerson(firstName, email, password, postalCode, street, lastName, phoneNumber, city, houseNumber, apartmentNumber) {
-  try {
-    const person = new Person({ firstName, email, password, postalCode, street, lastName, phoneNumber, city, houseNumber, apartmentNumber });
-    await person.save();
-    console.log('Osoba została dodana do bazy danych.');
-  } catch (error) {
-    console.error('Błąd podczas dodawania osoby:', error);
-  }
-}
-async function displayAllPersons() {
-  try {
-    const persons = await Person.find().exec(); // Pobierz wszystkie osoby z bazy danych
-    console.log('Wszystkie osoby w bazie danych:', persons);
-    return persons;
-  } catch (error) {
-    console.error('Błąd podczas pobierania osób:', error);
-    throw error;
-  }
-}
+const person = require("./controllers/person");
 
 app.get('/', (req, res) => {
   res.send('Work?! Connected to MongoDB!');
@@ -80,7 +31,7 @@ app.get('/persons', async (req, res) => {
     let tableHTML = req.query.status+'<br><table>';
     tableHTML += '<tr><th>Email</th><th>Imię</th><th>Nazwisko</th></tr>';
 
-    const persons = await displayAllPersons();
+    const persons = await person.displayAll();
     persons.forEach((person) => {
       tableHTML += `<tr><td>${person.email}</td><td>${person.firstName}</td><td>${person.lastName}</td></tr>`;
     });
@@ -111,7 +62,7 @@ app.post('/register', async (req, res) => {
 
   let status = 'Nie dodano';
   if (firstName && email && password && postalCode && street && lastName && phoneNumber && city && houseNumber) {
-    addPerson(firstName, email, password, postalCode, street, lastName, Number(phoneNumber), city, houseNumber, apartmentNumber);
+    person.add(firstName, email, password, postalCode, street, lastName, Number(phoneNumber), city, houseNumber, apartmentNumber);
     status = 'Dodano';
     //res.status(200).send('Register successful');
   }
