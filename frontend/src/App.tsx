a819a642fb8a8b7de03d84c96845700f8d0e3832
marcinpl1808 <marcinsikora123456@wp.pl>
2023-10-15 20:47:56 +0200

    MS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomePage from './components/HomePage';
+// import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import RegistrationPage from './components/RegistrationPage';
 import ResetPasswordPage from './components/ResetPasswordPage';
