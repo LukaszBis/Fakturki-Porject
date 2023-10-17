bcc3072e90c8aa64607212fbea214a63c8c9cfee
Lukasz Bis <lukasz.bis22@gmail.com>
2023-10-17 19:51:33 +0200

    lb edit

--- frontend/src/components/ConfirmEmailPage.tsx
+++ frontend/src/components/ConfirmEmailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import styles from './ConfirmEmailPage.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
