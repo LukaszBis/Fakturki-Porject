36112ffea1fc47565a35747b8d2f4dc9513cc8ac
Łukasz Biś <lukasz.bis22@gmail.com>
2023-11-27 16:11:08 +0100

    lb new ss for karuzela

--- frontend/src/components/welcomePage.tsx
+++ frontend/src/components/welcomePage.tsx
@@ -6,6 +6,7 @@ import Cookies from "js-cookie";
 import ButtonsLayoutLogout from "../layouts/ButtonsLayoutLogout.tsx";
 import ButtonsLayoutLogin from "../layouts/ButtonsLayoutLogin.tsx";
 import karuzela from "../assets/karuzela.png"
+import karuzela1 from "../assets/karuzela1.png"
 import karuzela2 from "../assets/karuzela2.png"
 import karuzela3 from "../assets/karuzela3.png"
 
@@ -65,16 +66,20 @@ const [buttons, setButtons] = useState(true);
                     <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active" draggable="false"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="1" draggable="false"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="2" draggable="false"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="3" draggable="false"></li>
                 </ol>
                 <div className="carousel-inner" id={styles.karuzelacenter}>
                     <div className="carousel-item active" >
                         <img className={styles.d_block} src={karuzela} alt="First slide" draggable="false"/>
                     </div>
                     <div className="carousel-item" >
-                        <img className={styles.d_block} src={karuzela2} alt="Second slide" draggable="false"/>
+                        <img className={styles.d_block} src={karuzela1} alt="Second slide" draggable="false"/>
                     </div>
                     <div className="carousel-item" >
-                        <img className={styles.d_block} src={karuzela3} alt="Third slide" draggable="false"/>
+                        <img className={styles.d_block} src={karuzela2} alt="Third slide" draggable="false"/>
+                    </div>
+                    <div className="carousel-item" >
+                        <img className={styles.d_block} src={karuzela3} alt="Fourth slide" draggable="false"/>
                     </div>
                 </div>
                 <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev" draggable="false">
